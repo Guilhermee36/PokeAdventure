4d88f7f285dd013d2a446a2de63ad11bad7a7144
Guilherme <guilhermelucario36@gmail.com>
2025-10-18 18:25:47 -0300

    ping pong test

--- MainBot.py
+++ MainBot.py
@@ -6,18 +6,26 @@ from dotenv import load_dotenv
 load_dotenv()
 Token = os.getenv('DISCORD_TOKEN')
 
-bot = discord.client(intents=discord.Intents.all())
+# Correção 1: 'discord.Client' com 'C' maiúsculo
+# Correção 2: Ativar as intents necessárias explicitamente é uma boa prática
+intents = discord.Intents.default()
+intents.message_content = True # Habilita a intent para ler o conteúdo das mensagens
+
+bot = discord.Client(intents=intents)
 
 @bot.event
-async def on_ready():   
+async def on_ready():
     print(f'Bot conectado como {bot.user}')
+
+# Correção 3: O evento 'on_message' deve estar fora do 'on_ready'
+@bot.event
+async def on_message(message):
+    # Ignora mensagens do próprio bot
+    if message.author == bot.user:
+        return
     
-    @bot.event
-    async def on_message(message):
-        if message.author == bot.user:
-            return
+    # Responde ao comando !ping
+    if message.content.startswith('!ping'):
+        await message.channel.send('Pong!')
         
-        if message.content.startswith('!ping'):
-            await message.channel.send('Pong!')
-            
 bot.run(Token)
